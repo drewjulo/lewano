e35e7980d48d274530cf6fe2441f4d31d0f2b617
drew julo <drewjulo@gmail.com>
2021-06-10 04:04:58 +0300

    Add files via upload

---
+++ site/assets/fs.js
@@ -0,0 +1,31 @@
+    function loadXMLDoc(theURL)
+        {
+            if (window.XMLHttpRequest)
+            {// code for IE7+, Firefox, Chrome, Opera, Safari, SeaMonkey
+                xmlhttp=new XMLHttpRequest();
+            }
+            else
+            {// code for IE6, IE5
+                xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+            }
+            xmlhttp.onreadystatechange=function()
+            {
+                if (xmlhttp.readyState==4 && xmlhttp.status==200)
+                {
+                    // alert(xmlhttp.responseText);
+                }
+            }
+            xmlhttp.open("GET", theURL, false);
+            xmlhttp.send();
+        }
+
+var xmlhttp=false;
+//loadXMLDoc('http://192.168.0.12:8080/list.txt');
+loadXMLDoc('/');
+if(xmlhttp==false)
+    { /* set timeout or alert() */ }
+else
+    { var contents = xmlhttp.responseText;
+    console.log(contents);
+    document.write(contents); }
+    
